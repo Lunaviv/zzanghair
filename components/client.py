70f33d0990e6c1cfe7a3dc13f4d7002f29d609f8
Ash <lunaviv98@gmail.com>
2025-12-26 04:25:15 +0900

    Attempt 1

--- components/client.py
+++ components/client.py
@@ -1,22 +1,20 @@
-class Client:   
+class Client:
     def __init__(self, name, age, sex):
         self.name = name
         self.age = age
         self.sex = sex
-    
+
     def show(self):
-        print(self.name, self.age, self.sex, sep=' ') 
+        print(self.name, self.age, self.sex, sep = " ")
 
 class ClientManager:
     def __init__(self):
         self.clients = []
-    
+
     def add(self, client):
         self.clients.append(client)
-    
+
     def show(self):
         for client in self.clients:
             client.show()
-    
-    # update
-    # delete
+            
--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-from components.client import Client, ClientManager
-from components.designer import Designer, DesignerManager
+from components.client import Client,ClientManager
+# from components.designer import
 
-def addClient(clientManager):
-    pass
+def showClient():
+    ClientManager.show()
 
+#
+def addClient():
+    #ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
+    newClient = Client(name, age, sex)
+    name = input("name: ")
+    age = input("age: ")
+    sex = input("sex: ")
+    
+    ClientManager.add(newClient)
+    
 def main():
+    print("미용실CRM")
+    cmd = input(">>")
+    error = "없는 메뉴입니다. help로 메뉴 목록을 확인하세요."
     clientManager = ClientManager()
-
-    cmd = input(">> ")
+    
+    #고객CRUD
     if cmd == "addc":
-        addClient(clientManager)
+        print("고객추가")
+        addClient()
+        main()
+    elif cmd == "readc":
+        print("고객목록")
+        ClientManager.show()
+        main()
+    elif cmd == "updc":
+        print("고객수정")
+    elif cmd == "delc":
+        print("고객삭제")
+    else:
+        print(error)
+        main()
 
-if __name__ == '__main__':
-    main()
+
+main()
